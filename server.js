baf0c72b293d621542b7a2656a856249f39f6965
channchealy <sek.channchealy@yahoo.com>
2022-08-27 21:40:19 -0500

    feature helpers

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 
+const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Tech blog',
