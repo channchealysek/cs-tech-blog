f31ccfc1ca8a7b7cdfe2d0373dba39a2b85f6b55
channchealy <sek.channchealy@yahoo.com>
2022-08-28 23:52:51 -0500

    login logout feature

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 // import all models
 const Post = require('./Post');
 const User = require('./User');
-const Vote = require('./Vote');
 const Comment = require('./Comment');
 
 // create associations
@@ -20,31 +19,6 @@ User.belongsToMany(Post, {
 
   foreignKey: 'user_id',
   onDelete: 'SET NULL'
-});
-
-Post.belongsToMany(User, {
-  through: Vote,
-  as: 'voted_posts',
-  foreignKey: 'post_id',
-  onDelete: 'SET NULL'
-});
-
-Vote.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-});
-
-Vote.belongsTo(Post, {
-  foreignKey: 'post_id',
-  onDelete: 'SET NULL'
-});
-
-User.hasMany(Vote, {
-  foreignKey: 'user_id'
-});
-
-Post.hasMany(Vote, {
-  foreignKey: 'post_id'
 });
 
 Comment.belongsTo(User, {
@@ -66,4 +40,4 @@ Post.hasMany(Comment, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote, Comment };
+module.exports = { User, Post, Comment };
