57e38a60c795033fa1df04b0c459bc9f2350000e
channchealy <sek.channchealy@yahoo.com>
2022-08-28 23:42:48 -0500

    helper feature

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,55 +2,25 @@ const { User } = require('../models');
 
 const userdata = [
   {
-    username: 'alesmonde0',
-    email: 'nwestnedge0@cbc.ca',
+    username: 'way1',
+    email: 'way1@gmail.com',
     password: 'password123'
   },
   {
-    username: 'jwilloughway1',
-    email: 'rmebes1@sogou.com',
+    username: 'way2',
+    email: 'way2@yahoo.com',
     password: 'password123'
   },
   {
-    username: 'iboddam2',
-    email: 'cstoneman2@last.fm',
+    username: 'way3',
+    email: 'way3@gmail.com',
     password: 'password123'
   },
   {
-    username: 'dstanmer3',
-    email: 'ihellier3@goo.ne.jp',
+    username: 'way4',
+    email: 'way4@hotmail.com',
     password: 'password123'
   },
-  {
-    username: 'djiri4',
-    email: 'gmidgley4@weather.com',
-    password: 'password123'
-  },
-  {
-    username: 'msprague5',
-    email: 'larnout5@imdb.com',
-    password: 'password123'
-  },
-  {
-    username: 'mpergens6',
-    email: 'hnapleton6@feedburner.com',
-    password: 'password123'
-  },
-  {
-    username: 'tpenniell7',
-    email: 'kperigo7@china.com.cn',
-    password: 'password123'
-  },
-  {
-    username: 'msabbins8',
-    email: 'lmongain8@google.ru',
-    password: 'password123'
-  },
-  {
-    username: 'jmacarthur9',
-    email: 'bsteen9@epa.gov',
-    password: 'password123'
-  }
 ];
 
 const seedUser = () => User.bulkCreate(userdata, {
