8cb8cbba78edfba5cbc42bd71144b11017b9a1c2
channchealy <sek.channchealy@yahoo.com>
2022-08-28 23:14:36 -0500

    comment feature

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,18 +3,18 @@ const { Post } = require('../models');
 const postdata =
 [
   {
-    postTitle: "Beautiful Day",
-    postContent: "This is a beautiful day",
+    postTitle: "if a string is equal to a value",
+    postContent: "Is it possible in Handlebars to check if a string is equal to another value without registering a helper?",
     user_id: 1
   },
   {
-    postTitle: "Donec diam",
-    postContent: "'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.",
+    postTitle: "What is Handlebars.js",
+    postContent: "Handlebars compiles templates into JavaScript functions. This makes the template execution faster than most other template engines.",
     user_id: 2
   },
   {
-    postTitle: "Do you hear it?",
-    postContent: "This is very musical, listen!",
+    postTitle: "Handlebars registerHelper serverside with Expressjs",
+    postContent: "Where to store custom Handlebars helpers in an Express app? Hot Network Questions · Did most or just few physicists think in 1900 that there was ...",
     user_id: 3
   }
 ];
